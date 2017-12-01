8be7661126d7989fe94d207e87afd952004ac432
TeeSeal <alex.theoddor@gmail.com>
2017-12-01 20:19:17 +0200

    Derp

--- src/commands/radio/stop.js
+++ src/commands/radio/stop.js
@@ -2,13 +2,13 @@ const { Command } = require('discord-akairo')
 
 class PlayCommand extends Command {
   constructor() {
-    super('play', {
-      aliases: ['play', 'join', 'change', 'switch'],
-      description: 'Pley ze station',
+    super('stop', {
+      aliases: ['stop'],
+      description: 'Stop ze pleyback',
     })
   }
 
-  async exec(msg, args) {
+  async exec(msg) {
     const connection = this.client.radio.connections.get(msg.guild.id)
     if (!connection) return msg.util.error('no active conneciton in this guild.')
     connection.stop()
