cf594057a2691f89b9b89e121c5e855aecb80c15
TeeSeal <alex.theoddor@gmail.com>
2017-12-05 20:05:24 +0200

    Remove logs

--- src/structures/radio/providers/Listen.moe.js
+++ src/structures/radio/providers/Listen.moe.js
@@ -30,9 +30,7 @@ class ListenMoe extends RadioProvider {
       const ws = new WebSocket('https://listen.moe/api/v2/socket')
       ws.once('open', () => { this.ws = ws })
       ws.on('message', data => {
-        console.log('[LISTEN.MOE]', data)
         if (!data) return
-        console.log('UPDATING')
         const firstMessage = !this.lastMessage
         this.lastMessage = JSON.parse(data)
         if (!firstMessage) this.handler.stations.get(this.id).refresh()
