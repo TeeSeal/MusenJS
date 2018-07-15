405dba27792206e164235a6a556579bd330aefb8
TeeSeal <teesealz@gmail.com>
2018-07-15 13:02:06 +0300

    update: main client to work with new discord-akairo

--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 require('dotenv').config()
 
-const { AkairoClient, CommandUtil } = require('discord-akairo')
-const { sequelize, Guild } = require('./db')
-const { ownerID } = require('./config')
+const { CommandUtil } = require('discord-akairo')
+const MusenClient = require('./struct/MusenClient')
 const logr = require('logr')
+const client = new MusenClient()
 
-const client = new AkairoClient({
-  prefix: msg => Guild.get(msg.guild ? msg.guild.id : 'dm', 'prefix'),
-  ownerID,
-  allowMention: true,
-  handleEdits: true,
-  automateCategories: true,
-  commandDirectory: 'src/commands/',
-  inhibitorDirectory: 'src/inhibitors/',
-  listenerDirectory: 'src/listeners/'
-})
-
-async function init () {
-  try {
-    logr.info('Connecting to database...')
-    await sequelize.sync()
-    logr.success('OK')
-
-    logr.info('Logging in...')
-    client.login(process.env.TOKEN)
-  } catch (err) {
-    throw err
-  }
-}
-init()
+client.init()
 
 process.on('unhandledRejection', err => {
   logr.error(err)
---
+++ src/struct/MusenClient.js
@@ -0,0 +1,50 @@
+const { sequelize, Guild } = require('./db')
+const { ownerID } = require('./config')
+const logr = require('logr')
+const {
+  AkairoClient,
+  CommandHandler,
+  InhibitorHandler,
+  ListenerHandler
+} = require('discord-akairo')
+
+class MusenClient extends AkairoClient {
+  constructor () {
+    super({ ownerID, allowMention: true, handleEdits: true })
+
+    this.commandHandler = new CommandHandler(this, {
+      prefix: msg => Guild.get(msg.guild ? msg.guild.id : 'dm', 'prefix'),
+      commandDirectory: 'src/commands/',
+      automateCategories: true
+    })
+
+    this.inhibitorHandler = new InhibitorHandler(this, {
+      inhibitorDirectory: 'src/inhibitors/',
+      automateCategories: true
+    })
+    this.commandHandler.useInhibitorHandler(this.inhibitorHandler)
+    this.inhibitorHandler.loadAll()
+
+    this.listenerHandler = new ListenerHandler(this, {
+      listenerDirectory: 'src/listeners/',
+      automateCategories: true
+    })
+    this.commandHandler.useListenerHandler(this.listenerHandler)
+    this.listenerHandler.loadAll()
+  }
+
+  async init () {
+    try {
+      logr.info('Connecting to database...')
+      await sequelize.sync()
+      logr.success('OK')
+
+      logr.info('Logging in...')
+      this.login(process.env.TOKEN)
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+module.exports = MusenClient
